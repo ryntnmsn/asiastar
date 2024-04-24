a8de11336a01326aa8fa3a5b8f7889269c8afcef
ryntnmsn <ryn.tnmsn@gmail.com>
2024-04-24 17:42:41 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', Login::class)->name('login');
